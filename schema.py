619b03d90488d93d62427f3cf35cd8e75f6442c8
Tahmid <47899219+TahmidMahin@users.noreply.github.com>
2021-06-05 22:06:07 +0600

    Update schema.py

--- schema.py
+++ schema.py
@@ -3,20 +3,29 @@ import sqlite3
 connection = sqlite3.connect('userinfo.db', check_same_thread = False)
 cursor = connection.cursor()
 
-cursor.execute(
-	'''Create Table users(
+cursor.execute('''
+	Create Table if not exists users(
 		username Varchar(16) Primary Key,
-		password Varchar(32) Check(length(password) > 6)
+		password Varchar(32) Check(length(password) > 5),
+		created Datetime
 		);'''
 	)
 
-cursor.execute(
-	'''Create Table activity(
+cursor.execute('''
+	Create Table if not exists activity(
 		username Varchar(16),
-		activity Varchar(100),
+		activity Text,
 		status Varchar(20) Check(status in ('Due', 'In progress', 'Done')),
 		Primary Key (username, activity),
 		Foreign Key (username) references users(username)
+		on Delete Cascade
+	);'''
+	)
+
+cursor.execute('''
+	Create Table if not exists admins(
+		admin Varchar(16) Primary Key,
+		password Varchar(32) Check(length(password) > 5)
 	);'''
 	)
 
